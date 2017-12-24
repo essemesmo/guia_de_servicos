9faba83734b920172b426454e9e397bccd9340de
Pedro <12203476+essemesmo@users.noreply.github.com>
2017-12-24 10:59:13 -0200

    Add files via upload

--- Projeto/src/Controlador/Senha.java
+++ Projeto/src/Controlador/Senha.java
@@ -1,11 +1,12 @@
 package Controlador;
 
-import sun.misc.BASE64Encoder;
+import java.util.Base64;
 
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.Arrays;
 import java.util.Random;
 
 /**
@@ -27,9 +28,8 @@ public class Senha
             String saltedAndHashed = password + "," + salt;
             MessageDigest digest = MessageDigest.getInstance("MD5");
             digest.update(saltedAndHashed.getBytes());
-            BASE64Encoder encoder = new BASE64Encoder();
             byte hashedBytes[] = (new String(digest.digest(), "UTF-8")).getBytes();
-            return encoder.encode(hashedBytes) + "," + salt;
+            return Arrays.toString(Base64.getEncoder().encode(hashedBytes)) + "," + salt;
             
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException("MD5 is not available", e);
@@ -50,9 +50,8 @@ public class Senha
             String saltedAndHashed = password + "," + salt;
             MessageDigest digest = MessageDigest.getInstance("MD5");
             digest.update(saltedAndHashed.getBytes());
-            BASE64Encoder encoder = new BASE64Encoder();
             byte hashedBytes[] = (new String(digest.digest(), "UTF-8")).getBytes();
-            return encoder.encode(hashedBytes) + "," + salt;
+            return Arrays.toString(Base64.getEncoder().encode(hashedBytes)) + "," + salt;
             
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException("MD5 is not available", e);
